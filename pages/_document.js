bdcecb9d1eb4ca29206f25d3a19e09d35edfe64d
ItsYasiru <mwmykd@gmail.com>
2022-06-22 14:19:23 +0530

    font fix for vercel

--- pages/_document.js
+++ pages/_document.js
@@ -21,14 +21,10 @@ class MyDocument extends Document {
                     />
 
                     <link
-                        href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap'
-                        rel='stylesheet'
-                    />
-                    <link
                         href='http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css'
                         rel='stylesheet'
                         type='text/css'
-                    ></link>
+                    />
                 </Head>
 
                 <body>
